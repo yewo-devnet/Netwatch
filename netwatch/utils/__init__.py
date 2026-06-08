0866b6343a6d80619aa03b54cbccefee50bdcf3a
Yewo Mkandawire <yewomkandawire1@gmail.com>
2026-06-08 23:21:36 +0200

    feat(utils): export public API from utils package __init__

--- netwatch/utils/__init__.py
+++ netwatch/utils/__init__.py
@@ -1,1 +1,9 @@
 """NetWatch utilities package."""
+
+from netwatch.utils.config import ConfigError, load_config, load_config_safe
+from netwatch.utils.logger import configure_root_logger, get_logger
+
+__all__ = [
+    "get_logger", "configure_root_logger",
+    "load_config", "load_config_safe", "ConfigError",
+]
